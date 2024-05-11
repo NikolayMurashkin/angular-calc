e60a055a9dfb27c6e6233a74da854fc956f80f91
NikolayMurashkin <m.nikolay1990@gmail.com>
2024-05-11 20:26:16 +0700

    add pipe lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 import { MyNavigationComponent } from './components/navigation/navigation.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { PostService } from './components/requests/services/post.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,6 @@ import { MyNavigationComponent } from './components/navigation/navigation.compon
   imports: [RouterOutlet, MyCalculatorComponent, MyNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  providers: [HttpClient, HttpClientModule, PostService],
 })
 export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { MyCalculatorComponent } from './my-calculator/my-calculator.component';
 import { NotFoundPage } from './components/notFoundPage/not-found-page.component';
 import { DirectivesPageComponent } from './components/directives/directives.component';
+import { LifecircleComponent } from './components/lifecircle/lifecircle.component';
+import { RequestsComponent } from './components/requests/requests.component';
+import { PipesComponent } from './components/pipes/pipes/pipes.component';
 
 export const routes: Routes = [
   {
@@ -23,6 +26,18 @@ export const routes: Routes = [
   {
     path: 'directives',
     component: DirectivesPageComponent,
+  },
+  {
+    path: 'lifecircle',
+    component: LifecircleComponent,
+  },
+  {
+    path: 'requests',
+    component: RequestsComponent,
+  },
+  {
+    path: 'pipes',
+    component: PipesComponent,
   },
   {
     path: '**',
